4729eafc2f467b4af7518c372870541974697eca
stelhs <stelhs@ya.ru>
2018-10-05 00:57:11 +0300

    implement actualization usio board state

--- usio_make_actions.php
+++ usio_make_actions.php
@@ -2,24 +2,35 @@
 <?php
 require_once '/usr/local/lib/php/common.php';
 require_once '/usr/local/lib/php/os.php';
+require_once 'common_lib.php';
 
 define("MSG_LOG_LEVEL", LOG_NOTICE);
 
 /* Calling by Usio daemon */
 function main($argv) {
-    if (count($argv) < 3) {
+    if (count($argv) < 2) {
         perror("incorrect arguments\n");
         return;
     }
 
-    $action_port = $argv[1];
-    $action_state = $argv[2];
+    $action = $argv[1];
+    switch ($action) {
+    case 'io_input':
+        $action_port = $argv[2];
+        $action_state = $argv[3];
+        $content = file_get_contents(sprintf("http://localhost:400/ioserver" .
+            "?io=usio1&port=%d&state=%d",
+            $action_port,
+            $action_state));
+        pnotice("returned content: %s\n", $content);
+        break;
 
-    $content = file_get_contents(sprintf("http://localhost:400/ioserver" .
-                                         "?io=usio1&port=%d&state=%d",
-                                         $action_port,
-                                         $action_state));
-    pnotice("returned content: %s\n", $content);
+    case 'restart':
+        $states_list = get_stored_io_states();
+        foreach ($states_list as $row)
+            httpio($row['io_name'])->relay_set_state($row['port'], $row['state']);
+        break;
+    }
 }
 
 
